80ad7ad1f3950e3a2d16ee22e7f864e8a9ecb0c5
alec sharp <alecsharpie@gmail.com>
2023-11-11 00:17:45 +1100

    refactor openai generate as function

--- src/explain.sh
+++ src/explain.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Check if the OPENAI_API_KEY is set
-if [[ -z "${OPENAI_API_KEY}" ]]; then
-  echo "The OPENAI_API_KEY environment variable is not set."
-  exit 1
-fi
+# Get the directory of the current script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "$DIR/llm/openai_generate.sh"
 
 # Check if git is installed
 if ! command -v git &>/dev/null; then
@@ -27,33 +25,7 @@ if [[ -z "${DIFF}" ]]; then
   exit 0
 fi
 
-# Escape the diff output for JSON
-ESCAPED_DIFF=$(jq -aRs . <<< "${DIFF}")
-
-# Prepare the data for the API request
-JSON_DATA=$(jq -n \
-  --arg model "gpt-3.5-turbo" \
-  --arg diff "$ESCAPED_DIFF" \
-  '{
-    "model": $model,
-    "messages": [
-      {"role": "system", "content": "You are a helpful assistant."},
-      {"role": "user", "content": ("Please summarize the following git diff.\nBe concise and holistic. Keep it very short. Do not provide any external commentary.\n\n" + ($diff | rtrimstr("\n")))}
-    ]
-  }')
-
-# Make the request to the OpenAI API
-RESPONSE=$(curl -s -X POST "https://api.openai.com/v1/chat/completions" \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer ${OPENAI_API_KEY}" \
-  -d "$JSON_DATA")
-
-# Check if the request was successful
-if [[ "$(echo "$RESPONSE" | jq -r '.error.message')" != "null" ]]; then
-  echo "Failed to get a response from the OpenAI API:"
-  echo "$RESPONSE" | jq -r '.error.message'
-  exit 1
-fi
-
-# Output the explanation
-echo "$RESPONSE" | jq -r '.choices[0].message.content'
+# Now you can use the openai_generate function
+DIFF=$(git diff)
+INSTRUCTION="Please summarize the following git diff.\nBe concise and holistic. Keep it very short. Do not provide any external commentary."
+openai_generate "$INSTRUCTION" "$DIFF"
